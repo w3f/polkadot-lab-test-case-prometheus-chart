89c1c95ccf09c506861285288a6253339c4f59d3
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-16 18:15:31 +0200

    initial implementation, missing getting metrics

--- src/test-case.ts
+++ src/test-case.ts
@@ -1,16 +1,41 @@
 import { Logger } from '@w3f/logger';
 import {
     TestCase,
-    LabResult
+    LabResult,
+    Status
 } from '@w3f/polkadot-lab-types';
 
 
+const name = 'number-of-peers';
+const period = 1000;
+
 export class NumberOfPeers implements TestCase {
+    private currentResult: LabResult;
+
     constructor(private readonly logger: Logger) { }
 
-    async start(): Promise<void> { }
+    async start(): Promise<void> {
+        const currentTime = Date.now().toString();
+        this.currentResult = {
+            name,
+            startTime: currentTime,
+            status: Status.InProgress,
+            data: []
+        };
+
+        const that = this;
+        setTimeout(async () => await that.getMetrics(), period);
+    }
 
     async result(): Promise<LabResult> {
-        return
+        const currentTime = Date.now().toString();
+
+        this.currentResult.endTime = currentTime;
+
+        return this.currentResult;
+    }
+
+    async getMetrics(): Promise<void> {
+        this.logger.debug('getMetrics called');
     }
 }
