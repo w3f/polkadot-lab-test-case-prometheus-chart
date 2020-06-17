e1e7af0f33dece6ebc00deec9db97f8f4aa7d496
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-17 16:24:07 +0200

    set first node as bootnode

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -22,14 +22,39 @@ main(){
     helm repo add w3f https://w3f.github.io/helm-charts/
     helm repo update
 
-    helm install -f ./scripts/values/prometheus-operator.yaml prometheus-operator stable/prometheus-operator
+    helm install -f ./scripts/values/prometheus-operator.yaml \
+         prometheus-operator stable/prometheus-operator
 
-    helm install -f ./scripts/values/polkadot-base-services.yaml  polkadot-base-services w3f/polkadot-base-services
+    helm install -f ./scripts/values/polkadot-base-services.yaml  \
+         polkadot-base-services w3f/polkadot-base-services
 
-    helm install -f ./scripts/values/polkadotyaml --set name=polkadot-1 --set p2pPort=30333 --set extraArgs.validator="--alice"  polkadot-1 w3f/polkadot
-    helm install -f ./scripts/values/polkadotyaml --set name=polkadot-2 --set p2pPort=30334 --set extraArgs.validator="--bob"  polkadot-2 w3f/polkadot
-    helm install -f ./scripts/values/polkadotyaml --set name=polkadot-3 --set p2pPort=30335 --set extraArgs.validator="--charly"  polkadot-3 w3f/polkadot
-    helm install -f ./scripts/values/polkadotyaml --set name=polkadot-4 --set p2pPort=30336 --set extraArgs.validator="--dave"  polkadot-4 w3f/polkadot
+    helm install -f ./scripts/values/polkadot.yaml \
+         --set name=polkadot-1 \
+         --set p2pPort=30333 \
+         --set extraArgs.validator="--alice" \
+         --set nodeKey="0000000000000000000000000000000000000000000000000000000000000000" \
+         polkadot-1 w3f/polkadot
+
+    helm install -f ./scripts/values/polkadot.yaml \
+         --set name=polkadot-2 \
+         --set p2pPort=30334 \
+         --set extraArgs.validator="--bob" \
+         --set extraBootnodes[0]="/dns4/polkadot-1-p2p/tcp/30333/p2p/12D3KooWDpJ7As7BWAwRMfu1VU2WCqNjvq387JEYKDBj4kx6nXTN" \
+         polkadot-2 w3f/polkadot
+
+    helm install -f ./scripts/values/polkadot.yaml \
+         --set name=polkadot-3 \
+         --set p2pPort=30335 \
+         --set extraArgs.validator="--charlie" \
+         --set extraBootnodes[0]="/dns4/polkadot-1-p2p/tcp/30333/p2p/12D3KooWDpJ7As7BWAwRMfu1VU2WCqNjvq387JEYKDBj4kx6nXTN" \
+         polkadot-3 w3f/polkadot
+
+    helm install -f ./scripts/values/polkadot.yaml \
+         --set name=polkadot-4 \
+         --set p2pPort=30336 \
+         --set extraArgs.validator="--dave" \
+         --set extraBootnodes[0]="/dns4/polkadot-1-p2p/tcp/30333/p2p/12D3KooWDpJ7As7BWAwRMfu1VU2WCqNjvq387JEYKDBj4kx6nXTN" \
+         polkadot-4 w3f/polkadot
 
     helm install test-case ./charts/lab-test-case-number-of-peers
 
