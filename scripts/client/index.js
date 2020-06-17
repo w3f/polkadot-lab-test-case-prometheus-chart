d47069b88663d6d89bd76ca59e47e402cf34c27c
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-17 21:12:23 +0200

    get result on test client

--- scripts/client/index.js
+++ scripts/client/index.js
@@ -2,4 +2,18 @@ const { Client } = require('../../dist/common');
 
 const client = new Client('ws://localhost:3000');
 
-client.start();
+function delay(delay) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, delay);
+  });
+}
+
+async function main() {
+  client.start();
+
+  await delay(5000);
+
+  const result = await client.requestStatus();
+}
+
+main();
