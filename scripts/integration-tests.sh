d8ebe5d690e7c97c0579822cd78227ea5ac9803c
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-18 13:17:49 +0200

    use helmfile for integration tests

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -3,13 +3,14 @@
 source /scripts/common.sh
 source /scripts/bootstrap-helm.sh
 
+WS_PORT=3000
 
 run_tests() {
     echo Running tests...
 
     wait_pod_ready test-case
 
-    kubectl port-forward svc/test-case 3000 &
+    kubectl port-forward svc/test-case ${WS_PORT} &
 
     yarn
     cd ./scripts/client
@@ -18,7 +19,7 @@ run_tests() {
 }
 
 teardown() {
-    echo helm del
+    echo helmfile del --purge
 }
 
 main(){
@@ -26,42 +27,7 @@ main(){
         trap teardown EXIT
     fi
 
-    helm repo add w3f https://w3f.github.io/helm-charts/
-    helm repo update
-
-    helm install -f ./scripts/values/prometheus-operator.yaml \
-         prometheus-operator stable/prometheus-operator
-
-    helm install -f ./scripts/values/polkadot-base-services.yaml  \
-         polkadot-base-services w3f/polkadot-base-services
-
-    helm install -f ./scripts/values/polkadot.yaml \
-         --set name=polkadot-1 \
-         --set p2pPort=30333 \
-         --set validator=true \
-         --set extraArgs.validator="--alice" \
-         --set nodeKey="0000000000000000000000000000000000000000000000000000000000000000" \
-         polkadot-1 w3f/polkadot
-
-    helm install -f ./scripts/values/polkadot.yaml \
-         --set name=polkadot-2 \
-         --set p2pPort=30334 \
-         --set extraBootnodes[0]="/dns4/polkadot-1-p2p/tcp/30333/p2p/12D3KooWDpJ7As7BWAwRMfu1VU2WCqNjvq387JEYKDBj4kx6nXTN" \
-         polkadot-2 w3f/polkadot
-
-    helm install -f ./scripts/values/polkadot.yaml \
-         --set name=polkadot-3 \
-         --set p2pPort=30335 \
-         --set extraBootnodes[0]="/dns4/polkadot-1-p2p/tcp/30333/p2p/12D3KooWDpJ7As7BWAwRMfu1VU2WCqNjvq387JEYKDBj4kx6nXTN" \
-         polkadot-3 w3f/polkadot
-
-    helm install -f ./scripts/values/polkadot.yaml \
-         --set name=polkadot-4 \
-         --set p2pPort=30336 \
-         --set extraBootnodes[0]="/dns4/polkadot-1-p2p/tcp/30333/p2p/12D3KooWDpJ7As7BWAwRMfu1VU2WCqNjvq387JEYKDBj4kx6nXTN" \
-         polkadot-4 w3f/polkadot
-
-    helm install --set port=3000 --set image.tag=${CIRCLE_SHA1} test-case ./charts/lab-test-case-number-of-peers
+    /scripts/build-helmfile.sh
 
     run_tests
 }
