3e8b749b1c6eb5d9247628b1580571015f85b413
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-21 12:43:54 +0200

    use mocha in integraion test

--- scripts/client/index.js
+++ scripts/client/index.js
@@ -1,20 +1,21 @@
+const { should } = require('chai');
+
 const { Client } = require('@w3f/polkadot-lab-test-case-common');
 const { createLogger } = require('@w3f/logger');
 
 const client = new Client('ws://localhost:3000');
+const logger = createLogger('debug');
 
+should();
 
-async function main() {
-  const logger = createLogger('debug');
+describe('Prometheus TestCase', () => {
+    it('should get results', async () => {
+      client.start();
 
-  client.start();
+      await client.delay(40000);
 
-  await client.delay(40000);
+      const result = await client.requestStatus();
 
-  const result = await client.requestStatus();
-
-  logger.debug(`result: ${JSON.stringify(result)}`);
-  process.exit(0);
-}
-
-main();
+      logger.debug(`result: ${JSON.stringify(result)}`);
+    });
+});
